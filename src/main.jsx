7534d7b1fc7c7a524be2af0e63a31d6017b7c0c1
unknown <dodonou3@gmail.com>
2026-04-30 10:27:36 -0300

    Melhorias de UX/UI

--- src/main.jsx
+++ src/main.jsx
@@ -8,3 +8,13 @@ createRoot(document.getElementById('root')).render(
     <App />
   </StrictMode>,
 )
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js').then(registration => {
+      console.log('ServiceWorker registrado com sucesso no escopo: ', registration.scope);
+    }).catch(err => {
+      console.log('Falha ao registrar o ServiceWorker: ', err);
+    });
+  });
+}
